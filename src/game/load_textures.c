18cc44c24333051f2ff10046f32f6ebfd55f0518
Caroline Bahia <carolinecbahia@gmail.com>
2026-08-14 01:59:21 -0300

    Add new textures and maps

--- src/game/load_textures.c
+++ src/game/load_textures.c
@@ -6,7 +6,7 @@
 /*   By: ccavalca <ccavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 16:34:37 by ccavalca          #+#    #+#             */
-/*   Updated: 2026/08/13 13:36:28 by ccavalca         ###   ########.fr       */
+/*   Updated: 2026/08/14 02:10:26 by ccavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ static mlx_texture_t	*load_single_texture(char *path)
 
 int	load_all_textures(t_game *game)
 {
-	char	*default_paths[4] = {"textures/walls/wall.png",
-		"textures/walls/wall.png", "textures/walls/wall.png",
-		"textures/walls/wall.png"};
-	int		i;
+    char    *default_paths[4] = {"textures/walls/arcane/ea.png",
+    "textures/walls/arcane/no.png", "textures/walls/arcane/so.png",
+    "textures/walls/arcane/we.png"};
+    int i;
 
 	i = 0;
 	while (i < 4)
